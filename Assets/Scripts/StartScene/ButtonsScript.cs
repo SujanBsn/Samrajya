b7da48e2e9fd1bd9cbacfb08582591a7715c2596
Sujan Basnet <basnetsujan2056@gmail.com>
2024-08-07 02:24:03 +0545

    UI updated UI particle effect added

--- Assets/Scripts/StartScene/ButtonsScript.cs
+++ Assets/Scripts/StartScene/ButtonsScript.cs
@@ -1,13 +1,26 @@
+using MoreMountains.Feedbacks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonsScript : MonoBehaviour
 {
+    public MMF_Player loadingSceneFeedback;
+
+
     /// <summary>
     /// Starts the first game scene
     /// </summary>
     public void StartGame()
     {
-        SceneManager.LoadScene("1_1");
+        loadingSceneFeedback.PlayFeedbacks();
+        //SceneManager.LoadScene("1_1");
+    }
+
+    /// <summary>
+    /// Exits the game
+    /// </summary>
+    public void EndGame()
+    {
+        Application.Quit();
     }
 }
